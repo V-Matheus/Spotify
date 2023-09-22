0a2be159eb4814ff5cf2b9ffcd80c69fa07027ed
Victor Matheus <victormatheus507@gmail.com>
2023-09-22 13:26:55 -0300

    replacing divs for buttons

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
 const songName = document.getElementById('song-name');
+const song = document.getElementById('audio');
 
-songName.innerText = 'Do I wanna know ?'
+songName.innerText = 'Do I wanna know ?';
+song.onpause()
