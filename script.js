3dc83354c58b98406f366b8c3ae67a46d4304d5c
Victor Matheus <victormatheus507@gmail.com>
2023-09-22 13:31:16 -0300

    add play music

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 const songName = document.getElementById('song-name');
 const song = document.getElementById('audio');
+const play = document.getElementById('play');
 
 songName.innerText = 'Do I wanna know ?';
-song.onpause()
+
+function playSong() {
+  song.play();
+}
+
+play.addEventListener('click', playSong);
