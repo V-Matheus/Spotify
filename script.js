9c4c4741d77720f169bb493b42b3185bf768c3e2
Victor Matheus <victormatheus507@gmail.com>
2023-09-25 12:56:46 -0300

    button play and pause

--- script.js
+++ script.js
@@ -3,9 +3,28 @@ const song = document.getElementById('audio');
 const play = document.getElementById('play');
 
 songName.innerText = 'Do I wanna know ?';
+let isPlay = false;
 
 function playSong() {
+  play.querySelector('.bi').classList.remove('bi-play-circle-fill');
+  play.querySelector('.bi').classList.add('bi-pause-circle-fill');
   song.play();
+  isPlay = true;
 }
 
-play.addEventListener('click', playSong);
+function pauseSong() {
+  play.querySelector('.bi').classList.remove('bi-pause-circle-fill');
+  play.querySelector('.bi').classList.add('bi-play-circle-fill');
+  song.pause();
+  isPlay = false;
+}
+
+function playPauseDecider() {
+  if(isPlay) {
+    pauseSong()
+  } else {
+    playSong()
+  }
+}
+
+play.addEventListener('click', playPauseDecider);
