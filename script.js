5031bc2e00d6c8b3bb62ebe8b8e1ea6a14bba8cd
Victor Matheus <victormatheus507@gmail.com>
2023-09-25 13:20:40 -0300

    creat object to the musics

--- script.js
+++ script.js
@@ -1,9 +1,38 @@
 const songName = document.getElementById('song-name');
+const bandName = document.getElementById('band-name');
 const song = document.getElementById('audio');
+const cover = document.getElementById('cover');
 const play = document.getElementById('play');
+const next = document.getElementById('next');
+const previous = document.getElementById('previous');
 
 songName.innerText = 'Do I wanna know ?';
+
+const arctic_monkeys_spotify = {
+  songName: 'Do I wanna know ?',
+  artist: 'Arctic Monkeys',
+  file: 'arctic_monkeys_spotify',
+};
+
+const master_of_puppets_spotify = {
+  songName: 'Master of Puppts ?',
+  artist: 'Metallica',
+  file: 'master_of_puppets_spotify',
+};
+
+const swee_child_spotify = {
+  songName: 'Sweet Child O Mine',
+  artist: "Guns N' Roses",
+  file: 'swee_child_spotify',
+};
+
 let isPlay = false;
+const playList = [
+  arctic_monkeys_spotify,
+  master_of_puppets_spotify,
+  swee_child_spotify,
+];
+let index = 0;
 
 function playSong() {
   play.querySelector('.bi').classList.remove('bi-play-circle-fill');
@@ -20,11 +49,20 @@ function pauseSong() {
 }
 
 function playPauseDecider() {
-  if(isPlay) {
-    pauseSong()
+  if (isPlay) {
+    pauseSong();
   } else {
-    playSong()
+    playSong();
   }
 }
+
+function initializeSong() {
+  cover.src = `img/${playList[index].file}.png`;
+  song.src = `songs/${playList[index].file}.mp3`;
+  songName.innerText = playList[index].songName;
+  bandName.innerText = playList[index].artist;
+}
+
+initializeSong()
 
 play.addEventListener('click', playPauseDecider);
